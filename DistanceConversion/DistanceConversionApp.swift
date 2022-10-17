06bea283a4e08e17893fa4721e1228a1f05bce7b
Stephen Houst <38192703+StephenHoust@users.noreply.github.com>
2022-10-17 13:25:19 -0700

    Initial Commit

---
+++ DistanceConversion/DistanceConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistanceConversionApp.swift
+//  DistanceConversion
+//
+//  Created by Stephen Houst on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct DistanceConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
